90f136bc54205c37d193c1a4fe9ee6df7f088b42
jagwarthegreat <jguapin21@gmail.com>
2021-05-19 14:04:58 +0800

    add config file

---
+++ src/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'sprnva',
+        'username' => 'root',
+        'password' => '',
+        'connection' => '127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    ],
+
+    'app' => [
+        'base_url' => 'sprnva',
+        'name' => 'SPRNVA',
+
+        // choices: windows, macOS, linux
+        'os' => 'macOS',
+    ]
+];
